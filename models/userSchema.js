bed2c77d4b13eb3e9dbcd880f5de22e593a3f459
Ram Swaroop Sahani <ramswaroopsahani@Rams-MacBook-Air.local>
2023-09-22 11:19:41 +0530

    schema updated

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,5 +1,4 @@
 const mongoose=require('mongoose');
-const jwt=require('jsonwebtoken');
 
 const userSchema=new mongoose.Schema({
     email:{
@@ -14,22 +13,11 @@ const userSchema=new mongoose.Schema({
         type:String,
         required:true
     },
-    cPassword:String,
     userType:String,
     tokens:[{
         token:String,
     }]
 })
 
-userSchema.methods.generateAuthToken=async function(){
-    try {
-        let token=jwt.sign({_id:this.id},process.env.secretKeyCookie)
-        this.tokens=this.tokens.concat({token:token})
-        await this.save()
-        return token	
-    } catch (error) {
-        console.log(error)
-    }
-}
 const user=mongoose.model('user',userSchema)
 module.exports= {user};
